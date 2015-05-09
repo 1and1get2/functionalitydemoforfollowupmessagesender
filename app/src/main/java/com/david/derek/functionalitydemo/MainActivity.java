802483915d6b6bc642ea16f8efe1c356e18a9f82
Derek Zhu <1and1get2@gmail.com>
2015-05-09 18:30:33 +1200

    add sms listener, need more work

--- app/src/main/java/com/david/derek/functionalitydemo/MainActivity.java
+++ app/src/main/java/com/david/derek/functionalitydemo/MainActivity.java
@@ -5,9 +5,13 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.provider.Telephony;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -152,5 +156,42 @@ public class MainActivity extends ActionBarActivity {
                     break;
             }
         }
+    }
+    public class SmsListener extends BroadcastReceiver{
+
+        private SharedPreferences preferences;
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // TODO Auto-generated method stub
+
+            if(intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")){
+                Bundle bundle = intent.getExtras();           //---get the SMS message passed in---
+                SmsMessage[] msgs = null;
+                String msg_from;
+                if (bundle != null){
+                    //---retrieve the SMS message received---
+                    try{
+                        Object[] pdus = (Object[]) bundle.get("pdus");
+                        msgs = new SmsMessage[pdus.length];
+                        for(int i=0; i<msgs.length; i++){
+                            msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                            msg_from = msgs[i].getOriginatingAddress();
+                            String msgBody = msgs[i].getMessageBody();
+                            Log.d("SMS onReceive", "Successful: " + msg_from + ":" + msgBody);
+                        }
+                    }catch(Exception e){
+                            Log.d("Exception caught",e.getMessage());
+                    }
+                }
+            }
+        }
+/*        public void onReceive(Context context, Intent intent) {
+            if (Telephony.Sms.Intents.SMS_RECEIVED_ACTION.equals(intent.getAction())) {
+                for (SmsMessage smsMessage : Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
+                    String messageBody = smsMessage.getMessageBody();
+                }
+            }
+        }*/
     }
 }
